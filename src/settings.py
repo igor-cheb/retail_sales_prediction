4ec71d394c5937bd6a30d092e106b1862f1815f8
igor-cheb <imadmeister@gmail.com>
2023-06-15 14:34:50 +0600

    test generation functionality drafted

--- src/settings.py
+++ src/settings.py
@@ -6,3 +6,6 @@ PROCESSED_PATH = 'data/processed/'
 
 #length of windows and shifts
 WINS_SHIFTS = [2, 6, 12]
+
+#functions to be calculated as window aggregates
+ROLL_FUNCS = ['sum', 'mean', 'std', 'min', 'max']
