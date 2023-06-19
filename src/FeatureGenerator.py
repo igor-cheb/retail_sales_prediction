6eea17bffab09db44870d8127929cae8deb68cff
igor-cheb <imadmeister@gmail.com>
2023-06-19 12:24:51 +0600

    expired code deleted

--- src/FeatureGenerator.py
+++ src/FeatureGenerator.py
@@ -10,7 +10,6 @@ class FeatureGenerator():
 
     def __init__(self, target_months: Optional[list]=None):
         self.merged_df = pd.read_parquet(PROCESSED_PATH + 'merged_train_df.parquet')
-        # self.merged_df = self.merged_df[self.merged_df['shop_id'].isin([26, 27, 28])]
 
         self.index_cols = ['shop_id', 'item_id', 'date_block_num']
         self.base_cols = ['item_price', 'item_cnt_day']
@@ -27,21 +26,6 @@ class FeatureGenerator():
     
     def _gen_base_features(self) -> pd.DataFrame:
         """Adding shop_id level feature aggregates"""
-        # local_df = self.merged_df[['date_block_num', 'shop_id', 'item_cnt_day', 'item_price']]\
-        #                 .reset_index(drop=True).copy()
-        # agg_di = {col: ROLL_FUNCS for col in base_cols}
-
-        # local_df = local_df.groupby(self.shop_group_cols, as_index=False).agg(agg_di)\
-        #                 .fillna(0)#.rename(columns={'item_id': 'deals'})
-        # local_df.columns = ['_'.join(col) if col[1] else col[0] for col in local_df.columns ]
-        
-        # # local_df = local_df.reset_index()
-        # local_df = self.shop_month_index_backbone.merge(local_df, 
-        #                                                 how='left', 
-        #                                                 on=self.shop_group_cols).fillna(0)
-        # self.base_feat_cols = [f'{col}_{agg}' for col in base_cols for agg in ROLL_FUNCS]
-        # return local_df
-
         local_df = self.merged_df[['date_block_num', 'item_id', 'shop_id', 
                                    'item_cnt_day', 'item_price']].reset_index(drop=True).copy()
 
@@ -62,17 +46,6 @@ class FeatureGenerator():
                     df: pd.DataFrame, 
                     cols_to_shift: list[str]) -> pd.DataFrame:
         """Adding lag features to the passed df based on passed shift_cols columns"""
-        # local_df = df.sort_values(self.shop_group_cols).copy()
-        # self.lag_cols = []
-        # for shift in SHIFTS + [0]:
-        #     for col in shift_cols:
-        #         if col in local_df:
-        #             new_col = f'{col}_shift_{shift}'
-        #             self.lag_cols.append(new_col)
-        #             # this assignment works b/c it's same as joining by index and local_df is sorted by shop_group_cols
-        #             local_df[new_col] = local_df.groupby('shop_id', as_index=False)[col].shift(periods=shift, fill_value=0)
-        # return local_df
-        # cols_to_shift = base_feat_cols + target_col
         self.shifted_cols = []
         for shift in SHIFTS:
             preshift_df = df[self.index_cols + cols_to_shift].copy()
@@ -87,20 +60,6 @@ class FeatureGenerator():
     def _add_rolling_windows(self,
                              df: pd.DataFrame) -> pd.DataFrame:
         """Adding window aggregates to the passed df based on passed cols_to_agg columns"""
-        # local_df = df.sort_values(self.shop_group_cols)
-
-        # self.roll_cols = []
-        # for func in ROLL_FUNCS:
-        #     for win_len in WINS:
-        #         for col in cols_to_roll:
-        #             if col in local_df:
-        #                 new_name = f'{col}_roll_{func}_{win_len}'
-        #                 self.roll_cols.append(new_name)
-        #                 # this assignment works b/c it's same as joining by index and
-        #                 # the dataset is ordered by shop and month
-        #                 local_df[new_name] = local_df.groupby('shop_id').rolling(win_len, min_periods=1)\
-        #                         .agg({col: func}).reset_index(drop=True).fillna(0)
-        # return local_df
         local_df = df.copy()
         self.roll_cols = []
         col = self.target_col[0]
