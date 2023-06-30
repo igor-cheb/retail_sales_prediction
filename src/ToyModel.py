719b8392beb6770350c9ad42f8cde02a09cb4eaf
igor-cheb <imadmeister@gmail.com>
2023-06-30 11:27:04 +0600

    toy model update

--- src/ToyModel.py
+++ src/ToyModel.py
@@ -5,7 +5,7 @@ class ToyModel():
     """Baseline model that predicts mean of the passed y vector at train time"""
 
     def fit(self, X: pd.DataFrame, y: pd.DataFrame):
-        self.y_mean = y.mean().values[0]
+        self.y_mean = y.mean()
 
     def predict(self, X):
         return np.array([self.y_mean] * X.shape[0])
